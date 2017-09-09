f602fa45a2c2987d948b0372688b3d4b92be287b
nycarts <jdalgety@gmail.com>
2017-09-08 22:03:19 -0500

    Making Tuples assignment added

---
+++ python/making_tuples.py
@@ -0,0 +1,11 @@
+def making_tuples():
+
+  my_dict = {
+    "Speros": "(555) 555-5555",
+    "Michael": "(999) 999-9999",
+    "Jay": "(777) 777-7777"
+  }
+
+  print my_dict.items()
+
+making_tuples()
