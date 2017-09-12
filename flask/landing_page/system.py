54a7d19c56122afc2f31b025ca9c39a02cf7912f
nycarts <jdalgety@gmail.com>
2017-09-11 21:56:07 -0500

    first part of Landing Page Flask assignment

---
+++ flask/landing_page/system.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+
+def index():
+
+  return render_template("index.html")
+
+
+
+@app.route("/ninjas")
+
+def ninjas():
+
+  return render_template("ninjas.html")
+
+  
+app.run(debug=True)
