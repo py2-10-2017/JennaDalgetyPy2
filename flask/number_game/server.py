af605f69d54397fb83c3e87004362cafb90b078d
nycarts <jdalgety@gmail.com>
2017-09-16 14:36:39 -0500

    Number Game, 1st draft

---
+++ flask/number_game/server.py
@@ -0,0 +1,39 @@
+
+from flask import Flask, render_template, session, redirect
+
+import random
+
+app = Flask(__name__)
+
+app.secret_key = "098hf7b5836b83724bfn3"
+
+
+@app.route("/")
+
+def index():
+
+    return render_template("index.html")
+
+
+app.route("/guess")
+
+def guess():
+
+    past_guesses = []
+
+    session["answer"] = 1
+
+    if session["guess"] in past_guesses:
+        pass
+    elif session["guess"] == session["answer"]:
+        return render_template(you_win.html)
+    else:
+        past_guesses.append(session["guess"])
+        if session["guess"] < session["answer"]:
+            return render_template("too_low.html")
+        elif session["guess"] > session["answer"]:
+            return render_template("too_high.html")
+
+
+
+app.run(debug=True)
