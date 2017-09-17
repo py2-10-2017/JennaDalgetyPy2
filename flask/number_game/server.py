da5cd5321d14b0f6f54a2590c73329a5d123ac55
nycarts <jdalgety@gmail.com>
2017-09-16 22:00:40 -0500

    Number Game, 7th draft

--- flask/number_game/server.py
+++ flask/number_game/server.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 
 app.secret_key = "098hf7b5836b83724bfn3"
 
-answer = random.randint(1, 10)
-
-past_guesses = []
-
 
 @app.route("/", methods=["POST", "GET"])
 
 def index():
+
+    if not "answer" in session:
+        session["answer"] = random.randint(1, 10)
+
 
     return render_template("index.html")
 
@@ -23,21 +23,16 @@ def index():
 
 def guess():
 
-    session["answer"] = answer
-
-    print("in Guess function")
-
     session["guess"] = int(request.form["guess"])
 
     if session["guess"] == session["answer"]:
+        session["answer"] = random.randint(1, 10)
         return render_template("you_win.html")
-    elif session["guess"] in past_guesses:
-        pass
     else:
-        past_guesses.append(session["guess"])
         if session["guess"] < session["answer"]:
             return render_template("too_low.html")
         elif session["guess"] > session["answer"]:
+
             return render_template("too_high.html")
 
 
