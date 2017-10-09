c22150ae6d3501ce28fb907c063b607200fa211f
nycarts <jdalgety@gmail.com>
2017-10-08 21:59:48 -0500

    Random Word Generator, v2

--- Py2/week_5_django/random_word_generator/apps/word_generator/views.py
+++ Py2/week_5_django/random_word_generator/apps/word_generator/views.py
@@ -4,10 +4,16 @@ from django.utils.crypto import get_random_string
 
 # Create your views here.
 def index(request):
+    try:
+        request.session["count"] += 1
+    except:
+        if not "count" in request.session:
+           request.session["count"] = 0
+
     context = {
         "word": get_random_string(length=14)
     } 
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', context, request.session.count)
 
 def generate_word(request):
     return redirect("/")
