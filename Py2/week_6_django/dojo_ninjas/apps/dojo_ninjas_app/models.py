4ce14499889745122d6e120dca5fa304c3bcd085
nycarts <jdalgety@gmail.com>
2017-10-14 00:12:39 -0500

    Dojo Ninjas assignment added

---
+++ Py2/week_6_django/dojo_ninjas/apps/dojo_ninjas_app/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    desc = models.TextField(blank=True, null=True)
+class Ninja(models.Model):
+    dojo = models.ForeignKey(Dojo, related_name="ninjas")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
