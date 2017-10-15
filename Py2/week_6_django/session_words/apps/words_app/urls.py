12d2ab505f40b995392b75b4112e1b89a67dedaf
nycarts <jdalgety@gmail.com>
2017-10-15 18:49:30 -0500

    Session Words v4

--- Py2/week_6_django/session_words/apps/words_app/urls.py
+++ Py2/week_6_django/session_words/apps/words_app/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'session_words$', views.index),
+    url(r'^session_words$', views.index),
     url(r'^choices$', views.choices),
     url(r'^clear$', views.clear)
 ]
--- Py2/week_6_django/session_words/apps/words_app/views.py
+++ Py2/week_6_django/session_words/apps/words_app/views.py
@@ -3,33 +3,31 @@ import datetime
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    
+    if "word" not in request.session:
+        request.session["word"] = []
 
-
-def choices(request):
-    if request.POST.get("word"):
-        request.session["word"] = request.POST.get("word")
-    if request.POST.get("color_red"):
-        request.session["color_red"] = request.POST.get("color_red")  
-    if request.POST.get("color_green"):
-        request.session["color_green"] = request.POST.get("color_green")
-    if request.POST.get("color_blue"):
-        request.session["color_blue"] = request.POST.get("color_blue") 
-    if request.POST.get("big_font"):
-        request.session["big_font"] = request.POST.get("big_font")
 
     context = {
         "words": request.session["word"],
-        "color_red": request.session["color_red"],
-        "color_green": request.session["color_green"],
-        "color_blue": request.session["color_blue"],
-        "big_font": request.session["big_font"],
         "time": datetime.datetime.now()
     }
 
-    print("through context")
+    return render(request, "index.html", context)
 
-    return redirect("/session_words", context)
+
+def choices(request):
+    request.session["word"].append(request.POST["word"])
+
+    styles = {
+        
+    }
+
+
+
+    request.session.modified = True
+
+    return redirect("/session_words")
 
 
 def clear(request):
