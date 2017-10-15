2e05844a846f523f8e7a00c64332abd42dabdbeb
nycarts <jdalgety@gmail.com>
2017-10-15 13:54:58 -0500

    Session Words v2

--- Py2/week_6_django/session_words/apps/words_app/views.py
+++ Py2/week_6_django/session_words/apps/words_app/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render, redirect, HttpResponse
 import datetime
+from django.utils.datastructures import MultiValueDictKeyError
 
 # Create your views here.
-def index(req):
-    return render(req, "index.html")
+def index(request):
+    return render(request, "index.html")
 
 
-def choices(req):
-    request.session["word"] = request.POST["word"]
-    request.session["color_red"] = request.POST["color_red"]
-    request.session["color_green"] = request.POST["color_green"]
-    request.session["color_blue"] = request.POST["color_blue"]
-    request.session["big_font"] = request.POST["big_font"]
+def choices(request):
+    try:
+        request.session["word"] = request.POST["word"]
+    except MultiValueDictKeyError: 
+        request.session["word"] = False
+    try:
+        request.session["color_red"] = request.POST["color_red"]
+    except MultiValueDictKeyError:
+        request.session["color_red"] = False
+    try:    
+        request.session["color_green"] = request.POST["color_green"]
+    except MultiValueDictKeyError:
+        request.session["color_green"] = False
+    try:    
+        request.session["color_blue"] = request.POST["color_blue"]
+    except MultiValueDictKeyError:
+        request.session["color_blue"] = False
+    try:    
+        request.session["big_font"] = request.POST["big_font"]
+    except MultiValueDictKeyError:    
+        request.session["big_font"] = False
 
     context = {
         "words": request.session["word"],
@@ -25,6 +41,6 @@ def choices(req):
     return redirect("/session_words", context)
 
 
-def clear(req):
+def clear(request):
     request.session.flush()
     return redirect("/session_words")
