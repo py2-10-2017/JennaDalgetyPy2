ff043641f163a58ba19a94bbd663ea533dc067c7
nycarts <jdalgety@gmail.com>
2017-10-18 21:58:26 -0500

    SemiRESTful Users v1

---
+++ Py2/week_7_django/semi_rest/apps/users/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from .models import User
+
+# Create your views here.
+def index(request):
+    users = User.objects.all()
+    context = {
+        "all_users": users
+    }
+    return render(request, "index.html", context)
+
+def create(request):
+    User.objects.create(first_name=request.POST["first_name"], last_name=request.POST["last_name"], email=request.POST["email"])
+    return redirect("/users")
+
+
+def new(request):
+    return render(request, "add_user.html")
+
+
+def show(request, id):
+    context = {
+        "user": User.objects.get(id=id)
+    }
+    return render(request, "show.html", context)
+
+
+def edit(request, id):
+    context = {
+        "user": User.objects.get(id=id)
+    }
+    return render(request, "edit.html", context)
+
+
+def destroy(request, id):
+    user_destroy = User.objects.get(id=id)
+    user_destroy.delete()
+
+    return redirect("/users")
+
+
